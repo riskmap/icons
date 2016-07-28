b643d94c7ab5f3fa565840e5edde33aa4157ef7e
willviles <will@willviles.com>
2016-07-28 11:53:25 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+
+  // Setup paths
+
+  function srcPath(path) { return './src/' + path; }
+
+  // Config Object
+
+  var config = {};
+
+  // Iconfonts
+
+  var iconfontName = 'riskmapicons',
+      prefix = 'ri',
+      pathFromStyles = './';
+
+  config.webfont = {
+    icons: {
+      src: srcPath('svgs/*.svg'),
+      dest: './dist',
+      options: {
+        font: iconfontName,
+        template: srcPath('style.css'),
+        templateOptions: {
+          fontFamilyName: iconfontName,
+          baseClass: iconfontName + '-icon',
+          classPrefix: prefix + '-',
+          mixinPrefix: prefix + '-',
+          extraStyles: true,
+          fontSrc: pathFromStyles + iconfontName
+        }
+      }
+    }
+  };
+
+  config.watch = {
+    iconfont: {
+      files: ['src/svg/*.svg'],
+      tasks: ['webfont:icons']
+    }
+  };
+
+  // Register individual tasks
+  grunt.registerTask('icons', [ 'webfont:icons' ]);
+  grunt.registerTask('watch', [ 'watch' ]);
+
+  // Register build task
+  grunt.registerTask('build', [
+    'icons', 'watch'
+  ]);
+
+  // Initialize configuration
+  grunt.initConfig(config);
+
+  // Load npm tasks
+  grunt.loadNpmTasks('grunt-webfont');
+
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+# RiskMap Icons
+
+This README outlines the details of collaborating to the RiskMap Icons iconfont.
+
+## Prerequisites
+
+You will need the following things properly installed on your computer.
+
+* [Git](http://git-scm.com/)
+* [Node.js](http://nodejs.org/) (with NPM)
+* [Bower](http://bower.io/)
+
+## Installation
+
+* `git clone <repository-url>` this repository
+* change into the new directory
+* `npm install`
+* `bower install`
+
+## Running / Development
+
+* `grunt build`
+* Add svgs to the src/svg folder and the iconfont will automatically build.
+
+## Further Reading / Useful Links
+
+* [grunt-webfont](https://github.com/sapegin/grunt-webfont/)
